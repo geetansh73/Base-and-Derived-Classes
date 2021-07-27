74196688abd46e20e2c4553fd3aa474dab99d3d6
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-07-27 11:38:53 +0530

    Create base.java

---
+++ base.java
@@ -0,0 +1,20 @@
+class base
+{
+int x;
+}
+class derived extends base
+{
+int y;
+void display()
+{
+System.out.print("ftde");
+}
+}
+class call
+{
+public static void main(String geetansh[])
+{
+derived d=new derived();
+d.display();
+}
+}
